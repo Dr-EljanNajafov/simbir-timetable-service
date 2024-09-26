87fa3dc78dd25780499b8f735cc4d72f52fad0bc
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-26 06:02:19 +0400

    enabling feign clients

--- src/main/java/com/github/simbir_timetable_service/SimbirTimetableServiceApplication.java
+++ src/main/java/com/github/simbir_timetable_service/SimbirTimetableServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.simbir_timetable_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SimbirTimetableServiceApplication {
 
     public static void main(String[] args) {
