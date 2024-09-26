b1a6febc774336aeb1ac8aaa0e20ba33576f950e
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-26 07:57:15 +0400

    enabling transaction management

--- src/main/java/com/github/simbir_timetable_service/SimbirTimetableServiceApplication.java
+++ src/main/java/com/github/simbir_timetable_service/SimbirTimetableServiceApplication.java
@@ -3,9 +3,11 @@ package com.github.simbir_timetable_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableTransactionManagement
 public class SimbirTimetableServiceApplication {
 
     public static void main(String[] args) {
