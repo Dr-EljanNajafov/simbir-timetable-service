acc9d30e37d66e914eb04bef2a96ffbbf6ced7c6
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-26 06:01:01 +0400

    adding migration

---
+++ src/main/resources/db/changelog/changeset/V001__simbir-timetable-service_timetable.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS timetable
+(
+    id          BIGSERIAL PRIMARY KEY,
+    hospital_id BIGINT       NOT NULL,
+    doctor_id   BIGINT       NOT NULL,
+    from_time   TIMESTAMP    NOT NULL,
+    to_time     TIMESTAMP    NOT NULL,
+    room        VARCHAR(255) NOT NULL,
+    CONSTRAINT check_duration CHECK (EXTRACT(EPOCH FROM (to_time - from_time)) / 60 <= 720), -- Ограничение: разница между from и to не более 12 часов
+    CONSTRAINT check_time_intervals CHECK (EXTRACT(MINUTE FROM from_time) % 30 = 0 AND
+                                           EXTRACT(MINUTE FROM to_time) % 30 =
+                                           0),                                               -- Минуты должны быть кратны 30, секунды всегда 0
+    CONSTRAINT check_from_to CHECK (to_time > from_time)                                     -- Ограничение: to должно быть больше from
+);
